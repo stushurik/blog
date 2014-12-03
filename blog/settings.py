640650a1bccf9c4072d035a949ce682c65b7c817
shurik <stu.shurik@gmail.com>
2014-12-03 16:27:35 +0200

    add  STATIC_ROOT

--- blog/settings.py
+++ blog/settings.py
@@ -89,6 +89,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), )
 
